2c8fa66e310776e334cdd5fb721fda2b331dd12a
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-02-11 23:39:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
       end
   end
 
-   def self.count_all_students_in_grade_9
+   def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 9
