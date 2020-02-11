0c36b6190a1dc2d9b00c5bb15cb2bd05f06b05cc
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-02-11 23:38:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,6 +48,40 @@ class Student
       end
   end
 
+   def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade IS NOT 12
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+  end
+
+  def self.first_x_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?
+      SQL
+
+      DB[:conn].execute(sql, num).map do |row|
+        self.new_from_db(row)
+      end
+  end
+
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
